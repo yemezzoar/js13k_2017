79aa29414453fa823869a80ea115340a31867d91
yemezzoar <yemezzoar@gmail.com>
2017-09-10 21:47:55 +0800

    more development

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 var fc = 0,
   sce = 0,
-  lvl = 3,
+  lvl = 1,
   tileSize = 50,
   code = -1,
   keypressed = 0;
@@ -9,6 +9,7 @@ window.addEventListener("keyup", this.keyUp, 0);
 
 function keyDown(e) {
   code = e.keyCode;
+  //console.log(code);
   keypressed = !event.repeat;
 }
 
@@ -27,8 +28,7 @@ var myGameArea = {
     this.cv.width = tileSize * 16;
     this.cv.height = tileSize * 12;
     this.ctx = this.cv.getContext("2d");
-    this.ctx.font = "30px Courier";
-    this.ctx.textAlign="center";
+    this.ctx.textAlign = "center";
     document.body.insertBefore(this.cv, document.body.childNodes[0]);
     this.interval = setInterval(updateGameArea, 20);
     this.title = new title(this.cv.width / 2, this.cv.height / 2);
@@ -38,6 +38,7 @@ var myGameArea = {
     this.ctx.setTransform(1, 0, 0, 1, 0, 0);
     this.ctx.fillStyle = "#000";
     this.ctx.fillRect(0, 0, this.cv.width, this.cv.height);
+    this.ctx.font = "20px Courier";
   },
   update: function() {
     switch (sce) {
@@ -47,7 +48,13 @@ var myGameArea = {
         break;
       case 1:
         this.level.update();
-        if (keypressed) this.level.fade = 1;
+        if (keypressed) {
+          if (this.level.state == 0) this.level.fade = 1;
+          if (this.level.state == 3) {
+            if (code == 89) this.level.showAns(0);
+            if (code == 78) this.level.showAns(1);
+          }
+        }
         break;
       case 2:
         this.level.lvlUp();
@@ -66,7 +73,8 @@ function updateGameArea() {
 }
 
 function title(x, y) {
-  var a = 1, pos = [5, -49, -45, -39, 63, -36, 60, -58, 102, -37, -84, -23, -50, -30];
+  var a = 1,
+    pos = [0, -49, -50, -39, 58, -36, 55, -58, 97, -37, -89, -23, -55, -30];
   this.fade = 0;
   this.update = function() {
     ctx = myGameArea.ctx;
@@ -77,7 +85,7 @@ function title(x, y) {
         sce++;
       }
     }
-    var d = Math.floor(fc / 10) % 10;
+    var d = Math.floor(fc / 10) % 10; //displacement for floating animation
     if (d > 4) {
       d = 9 - d;
     }
@@ -95,45 +103,106 @@ function title(x, y) {
       );
       ctx.closePath();
     }
-    //if (best == 0){
     ctx.fillText("Press any key to start.", x, y + 100);
-    //}else{
-      //ctx.fillText("Best Level: "+prevLvl, x, y + 100);
-    //}
+    ctx.font = "15px Courier";
+    if (lvl > 1) {
+      ctx.fillText("Best: " + lvl + " steps away", x, y - 150);
+    }
     ctx.fillStyle = "#000";
-    ctx.fillText("L O S T", x, y + pos[13] + d);
+    ctx.font = "20px Courier";
+    ctx.fillText("Way Home", x, y + pos[13] + d);
   };
 }
 
-function house() {
+/*
+* House object
+*/
+function house(s = 0, X = 0, Y = 0) {
+  this.state = 0;
+  this.delete = 0;
+  var x = X,
+    y = Y,
+    t = 0;
   this.draw = function() {
     ctx = myGameArea.ctx;
     ctx.fillStyle = "#fff";
-    ctx.beginPath();
-    ctx.moveTo(1, tileSize / 2);
-    ctx.lineTo(tileSize - 1, tileSize / 2);
-    ctx.lineTo(tileSize / 2, 1);
-    ctx.fill();
-    ctx.closePath();
-    ctx.fillRect(
-      tileSize / 4 - 4,
-      tileSize / 2,
-      tileSize / 2 + 8,
-      tileSize / 2 - 2
-    );
-    ctx.fillStyle = "#000";
-    ctx.fillRect(
-      tileSize / 4 + 4,
-      tileSize / 2 + 2,
-      tileSize / 2 - 8,
-      tileSize / 2 - 4
-    );
+    switch (this.state) {
+      case 0: //a house
+        //triangle
+        ctx.beginPath();
+        ctx.moveTo(1, tileSize / 2);
+        ctx.lineTo(tileSize - 1, tileSize / 2);
+        ctx.lineTo(tileSize / 2, 1);
+        ctx.fill();
+        ctx.closePath();
+        //sqaure
+        ctx.fillRect(
+          tileSize / 4 - 4,
+          tileSize / 2,
+          tileSize / 2 + 8,
+          tileSize / 2 - 2
+        );
+        ctx.fillStyle = "#000";
+        //door
+        ctx.fillRect(
+          tileSize / 4 + 4,
+          tileSize / 2 + 2,
+          tileSize / 2 - 8,
+          tileSize / 2 - 4
+        );
+        break;
+      case 1: //404 not found
+        console.log(fc - t);
+        if (fc - t > 240) {
+          this.delete = 1;
+        }
+        ctx.strokeStyle = "#fff";
+        ctx.lineWidth = 1;
+        ctx.strokeRect(x, y, tileSize, tileSize);
+        ctx.fillRect(x + 4, y + 4, tileSize / 4, tileSize / 4);
+        ctx.strokeStyle = "#000";
+        ctx.beginPath();
+        ctx.moveTo(x + 6, y + 6);
+        ctx.lineTo(x + tileSize / 4, y + tileSize / 4);
+        ctx.stroke();
+        ctx.closePath();
+        ctx.beginPath();
+        ctx.moveTo(x + 6, y + tileSize / 4);
+        ctx.lineTo(x + tileSize / 4, y + 6);
+        ctx.stroke();
+        ctx.closePath();
+        break;
+      case 2: //loading
+        var a = Math.floor((fc % 80) / 10) + 1; //shifting opacity values
+        for (var i = a; i < a + 8; i++) {
+          x + (tileSize / 2 - 4) * Math.sin(i * 2 * Math.PI / 8),
+            y + (tileSize / 2 - 4) * Math.sin(i * 2 * Math.PI / 8);
+          ctx.fillStyle = "rgba(200,200,200," + (i - a) * 1 / 8 + ")";
+          ctx.beginPath();
+          ctx.arc(
+            x +
+              tileSize / 2 +
+              (tileSize / 2 - 4) * Math.sin(i * 2 * Math.PI / 8),
+            y +
+              tileSize / 2 +
+              (tileSize / 2 - 4) * Math.cos(i * 2 * Math.PI / 8),
+            tileSize / 8 * ((i - a) / 8),
+            0,
+            2 * Math.PI,
+            false
+          );
+          ctx.fill();
+          ctx.closePath();
+        }
+        break;
+    }
   };
 }
 
 function player(X, Y) {
-  this.home = false;
-  this.a = 1;
+  //draws player
+  this.goHome = 0;
+  var a = 1;
   this.dX = 0;
   this.dY = 0;
   this.x = X;
@@ -146,7 +215,7 @@ function player(X, Y) {
         lvl++;
       }
     }
-    ctx.fillStyle = "rgba(200,200,200," + this.a + ")";
+    ctx.fillStyle = "rgba(255,255,255," + a + ")";
     ctx.beginPath();
     ctx.arc(
       this.x * tileSize + tileSize / 2,
@@ -168,6 +237,11 @@ function player(X, Y) {
       this.dX = 0;
       this.dY = 0;
     }
+    if (this.x == 0 && this.y == 0 && a > 0) {
+      a -= 0.02;
+    } else {
+      this.fade = 0;
+    }
   };
   this.move = function(nPos, time) {
     this.nX = nPos.x;
@@ -178,34 +252,33 @@ function player(X, Y) {
   this.still = function() {
     return this.dX == 0 && this.dY == 0;
   };
-  this.goHome = function() {
-    this.home = 1;
-  };
 }
 
 function level(cx, cy) {
   var a = 1,
-    state = 0,
     playerTurn = 0,
-    stepsTaken = 0;
+    stepsTaken = 0,
+    showAns = 0;
+  t = 0;
   var pastD = [],
     pPos = [{ x: 0, y: 1 }],
     now = { x: 0, y: 1 },
     e = [{ x: -1, y: 0 }, { x: 0, y: -1 }, { x: 1, y: 0 }, { x: 0, y: 1 }];
   this.fade = 0;
+  this.state = 0;
   this.p = new player(now.x, now.y);
   this.h = new house();
   this.lvlUp = function() {
     a = 1;
     this.fade = 0;
-    state = 0;
+    this.state = 0;
     playerTurn = 0;
     lvl++;
     stepsTaken = 0;
     sce = 1;
   };
   this.pMove = function() {
-    if (state == 1 && keypressed && code > 36 && code < 41) {
+    if (this.state == 1 && keypressed && code > 36 && code < 41) {
       var d = e[code - 37];
       now.x += d.x;
       now.y += d.y;
@@ -213,7 +286,7 @@ function level(cx, cy) {
       if (JSON.stringify(now) === JSON.stringify(pPos[pPos.length - 2])) {
         pPos.pop();
       } else {
-        state = 3;
+        this.state = 3;
       }
     }
   };
@@ -257,7 +330,7 @@ function level(cx, cy) {
     }
     var nPos;
     if (newPos.length > 1) {
-      nPos = newPos[rand(newPos.length)];
+      nPos = newPos[Math.floor(Math.random() * newPos.length)];
     } else {
       nPos = newPos[0];
     }
@@ -279,10 +352,16 @@ function level(cx, cy) {
       playerTurn = 1;
     }
   };
+  this.goHome = function() {
+    //animates player token going home
+    pPos.unshift({ x: 0, y: 0 });
+    this.p.move(pPos[pPos.length - 1], 1000);
+    pPos.pop();
+  };
   this.update = function() {
-    if (state == 1) {
+    if (this.state == 1) {
       if (playerTurn && pPos.length == 1) {
-        state = 2;
+        this.state = 2;
       }
       if (this.p.still()) {
         if (playerTurn) {
@@ -292,40 +371,115 @@ function level(cx, cy) {
         }
       }
     }
-    if (state == 2 && this.p.still() && keypressed) sce = 2;
+    if (this.state == 2 && this.p.still() && keypressed) sce = 2;
+    if (this.state == 4) {
+      if (this.nh != undefined) {
+        //show for 4 seconds before restarting game
+        console.log(fc - t);
+        if (fc - t > 80) {
+          this.h = this.nh;
+          this.h.state = 0;
+          lvl = 0;
+          this.levelUp();
+        }
+      } else {
+        if (this.p.still()) {
+          if (pPos.length > 0) {
+            //player token travels back to home and restart current level
+            this.goHome();
+          } else{
+            myGameArea.restart();
+          }
+        }
+      }
+    }
     this.p.update();
     this.draw();
   };
   this.draw = function() {
     ctx = myGameArea.ctx;
-    if (state == 0) {
+    if (this.state == 0) {
       if (this.fade) {
         if (a > 0) {
           a -= 0.01;
         } else {
-          state++;
+          this.state++;
         }
       }
       ctx.fillStyle = "rgba(200,200,200," + a + ")";
-      ctx.fillText("Level " + lvl, cx, cy-150);
+      ctx.fillText("Level " + lvl, cx, cy - 150);
     } else {
       ctx.fillStyle = "#fff";
-      if (state == 2) {
+      if (this.state == 2) {
         ctx.fillText("Press any key to go to next level.", cx, cy + 100);
       }
-      if (state == 1 && lvl < 3) {
-        //show instructions
-        var pos = [-60, -20, -20, 20, 60, -20, -20, -20];
+      if (this.state == 1 && lvl < 4 && pPos.length > 1) {
+        //show arrow keys (tutorial)
+        var arrowPos = [-55, 0, -15, -40, 25, 0, -15, 0];
+        var ans = {
+          x: pPos[pPos.length - 2].x - now.x,
+          y: pPos[pPos.length - 2].y - now.y
+        };
+        var arrow = e.findIndex(function(d) {
+          return ans.x == d.x && ans.y == d.y;
+        });
         for (var i = 0; i < 4; i++) {
-          if (i == arrow) {
-            //blink
+          if (i == arrow && playerTurn && this.p.still()) {
+            ctx.fillStyle =
+              "rgba(200,200,200," + Math.floor((fc % 60) / 30) + ")";
+            ctx.strokeStyle = ctx.fillStyle;
+          } else {
+            ctx.fillStyle = "#333";
+            ctx.strokeStyle = "#333";
           }
-          ctx.fillRect(x + pos[i * 2], y + pos[i * 2 + 1], 38, 38);
+          ctx.fillRect(
+            cx + arrowPos[i * 2],
+            cy + arrowPos[i * 2 + 1] + 150,
+            30,
+            30
+          );
+          ctx.lineJoin = "round";
+          ctx.lineWidth = 5;
+          ctx.strokeRect(
+            cx + arrowPos[i * 2],
+            cy + arrowPos[i * 2 + 1] + 150,
+            30,
+            30
+          );
         }
       }
-      if (state == 3){
-        ctx.fillText("LOST!" + lvl, cx, cy-50);
+      if (this.state == 3) {
+        ctx.fillText("Is home where you are? (y/n)", cx, cy + 140);
       }
+    }
+    if (showAns) {
+      //reveal right path home and animate player token going home
+      ctx.setTransform(
+        1,
+        0,
+        0,
+        1,
+        cx - (this.p.x * tileSize + tileSize / 2),
+        cy - (this.p.y * tileSize + tileSize / 2)
+      );
+      if (pPos.length > 1) {
+        for (var i = 1; i < pPos.length; i++) {
+          ctx.beginPath();
+          ctx.arc(
+            pPos[i].x * tileSize + tileSize / 2,
+            pPos[i].y * tileSize + tileSize / 2,
+            tileSize / 8,
+            0,
+            2 * Math.PI,
+            false
+          );
+          ctx.fill();
+          ctx.closePath();
+        }
+      }
+    }
+    if (this.nh != undefined) {
+      this.nh.draw();
     }
     ctx.setTransform(
       1,
@@ -338,10 +492,18 @@ function level(cx, cy) {
     this.h.draw();
     this.p.draw();
   };
-}
-
-function rand(n) {
-  return Math.floor(Math.random() * n);
+  this.showAns = function(b) {
+    if (b == 0) {
+      //existing home becomes 404 and new home token constructed
+      this.h.state = 1;
+      this.nh = new house(this.p.x, this.p.y - 1, 2);
+      t = fc;
+    } else if (b == 1) {
+      //show way back home
+      showAns = 1;
+    }
+    this.state++;
+  };
 }
 
 function is_same(a1, a2) {
